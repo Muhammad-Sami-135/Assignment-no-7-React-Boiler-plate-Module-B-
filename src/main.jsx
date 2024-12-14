ef8eeed2d429a77f79c2943c7943e0740ea60d63
Muhammad-Sami-135 <muhammadsamialam8@gmail.com>
2024-12-15 04:02:55 +0500

    react boiler plate

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>Name</h1>
+    <h3>Muhammad Sami</h3>
+    <h1>Paragraph</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores unde non illo adipisci labore earum voluptate facere impedit delectus repudiandae deserunt, voluptatum aperiam asperiores tenetur aut esse possimus rerum neque tempora dolorum officiis numquam aliquam exercitationem debitis! Quaerat molestiae omnis fugit quasi hic, at doloremque minus cupiditate voluptatem, velit cumque odit libero corporis totam consequuntur reprehenderit minima magnam culpa deserunt. Obcaecati impedit placeat illo ad eaque aperiam, unde beatae velit quasi consectetur repellat autem ut iusto cum in molestiae veritatis minus praesentium aspernatur ab eius qui odio? Nihil blanditiis quibusdam, nisi tempora deserunt debitis illo ad vitae. Consequatur explicabo recusandae deleniti eveniet quis, ipsam inventore molestiae non minima numquam, id laboriosam veritatis tempora veniam culpa, voluptatibus repellendus error harum quisquam molestias praesentium voluptatem? Voluptatum itaque impedit in, doloribus similique velit quis enim ex culpa vitae quasi reprehenderit dolor modi eaque necessitatibus dignissimos doloremque neque sint, illo quaerat esse eum molestiae minus. Voluptatum ratione totam animi, architecto nam reiciendis sequi similique fugit excepturi, placeat at natus maiores enim ducimus reprehenderit blanditiis ullam fugiat tempora repudiandae, ea necessitatibus minima voluptatem optio! Ad molestias minus maiores, repellendus, rem alias commodi velit inventore officia laboriosam, culpa quibusdam non earum explicabo omnis laborum reprehenderit totam.</p>
+    <h1>Images</h1>
+    <img src="/public/vite.svg" width="200" alt="" />
+    <img src="/src/assets/react.svg" width="200" alt="" />
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1dzAY9vxSYgd7Zz6Aji9j2-LaG3-BF5iw5w&s" width="200" alt="" />
+  </div>
+)
